804f46be752c5dd670fb8866a99ebad252abc9c1
dubey <dubeysaksham801@gmail.com>
2025-10-15 17:47:48 +0530

    update the procfile2

--- Trading_Journal/wsgi.py
+++ Trading_Journal/wsgi.py
@@ -1,10 +1,6 @@
-# WSGI entrypoint for the Django project
-# This file proxies to the actual Django WSGI application located at
-# Trading_Journal/Trading_Journal/wsgi.py to ensure gunicorn imports the
-# correct module when using the path "Trading_Journal.wsgi:application".
+import os
+from django.core.wsgi import get_wsgi_application
 
-from importlib import import_module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trading_Journal.settings')
 
-# Import the inner project's wsgi module and expose its `application`
-_inner_wsgi = import_module('Trading_Journal.Trading_Journal.wsgi')
-application = getattr(_inner_wsgi, 'application')
+application = get_wsgi_application()
